9e047996cb0560a7aab25f12f06e5b2c91f0bb33
googol88 <ronaldfleung@gmail.com>
2021-03-19 21:19:17 -0400

    work on text styling

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@
 /** @jsx jsx */
 import { 
   jsx,
-  Themed,
+  Box,
   Heading,
   Text,
   Button, 
@@ -11,14 +11,19 @@ import {
 
 const Index = (props) => (
   <Container as="main" py={4} variant="copy" {...props}>
-    <Heading variant="ultratitle">hillsHacks IV</Heading>
+    <Box bg="slate" color="white">
+      <Heading
+        variant="ultratitle">
+          hillsHacks IV
+      </Heading>
+    </Box>
 
-    <Heading variant="subtitle">There's never been a better time to build. And the world has never needed it more than now.</Heading>
+    <Heading variant="subtitle">There's never been a better time to build. <strong>And the world has never needed it more than now.</strong></Heading>
 
     <Text variant="lead">This is the fourth iteration of hillsHacks, Watchung Hills' premier hackathon open to grades 7 through 12.</Text>
     <br/>
-    <Button as="a"
-href="https://github.com/lachlanjc/next-theme-starter">GitHub</Button>
+    <Button as="a" mt={16}
+href="https://github.com/lachlanjc/next-theme-starter">Register</Button>
   </Container>
 )
 
