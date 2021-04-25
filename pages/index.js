2a2139d95b7c907174f874606f958cb37d3bfc6c
googol88 <ronaldfleung@gmail.com>
2021-04-25 09:38:25 -0400

    💄 Adust container size and replace components

--- pages/index.js
+++ pages/index.js
@@ -22,11 +22,32 @@ const Index = (props) => (
   href="https://github.com/lachlanjc/next-theme-starter">Register</Button>
       </Container>
     </Box>
-    <Container as="main" py={4} variant="copy" {...props}>
+    <Container as="section" py={4} variant="copy" {...props}>
       <Heading variant="headline">This is not your typical hackathon.</Heading>
-      <Text as="p" mb={3}><b>hillsHacks was born with a mission:</b> to show the world that computer science is more than memorizing technical jargon and stuffing code into machines. CS is for innovators, creators, problem solvers, artists, and YOU. And at hillsHacks, we'll show you the ropes .</Text>
-      <Text as="p" mb={3}>Rather than leave you alone for 12 hours expecting you to come out with __, we'll guide you through with workshops from Watchung Hills alumni, and implement your ideas in our design challenge without code, so you won't have to stress over the details.</Text>
+      <Text as="p" mb={3}><b>hillsHacks was born with a mission:</b> to show the world that computer science is more than memorizing technical jargon and stuffing code into machines. CS is for innovators, creators, problem solvers, artists, and <i>you</i>. And at hillsHacks, we'll show you the ropes .</Text>
+      <Text as="p" mb={3}>Rather than leave you alone for 12 hours expecting you to come out with __, we'll guide you through with workshops from Watchung Hills alumni and provide one-on-one mentorship. You'll implement your ideas in our design challenge without code, so you won't have to stress over the details.</Text>
       <Text variant="lead">This is the fourth iteration of hillsHacks, Watchung Hills' premier hackathon open to grades 6 through 12.</Text>
+      <br/>
+    </Container>
+    <Container as="section" py={4} {...props}>
+      <Heading variant="headline">FAQ</Heading>
+      <h2>Why come to hillsHacks?</h2>
+<p>Can you spend a day without using some form of technology developed in the last 5 years? It would probably be difficult - technology has completely changed the way we live our lives. Regardless of your experience, you will learn something new at hillsHacks about how computer science is being used all around us - often in things we take for granted! All students grade 6 and up are welcome.</p>
+
+<h2>What if I don’t know anything about coding or computer science?</h2>
+<p>No problem. We love beginners! There's a lot to learn at hillsHacks, and we hope you will leave inspired.</p>
+
+<h2>I’m an expert at coding. What do I get out of this event?</h2>
+<p>Take a look at some of our advanced workshops like ___!</p>
+
+<h2>This sounds like a long day, will there be food?</h2>
+<p>Of course! All participants will be given lunch and dinner. Snacks will also be available throughout the day.</p>
+
+<h2>Do I need to bring my own computer?</h2>
+<p>You are welcome to bring own computer. If you forget yours, don't worry, we have Chromebooks for everyone to use!</p>
+
+<h2>Sounds great! Where is it?</h2>
+<p>It's at Watchung Hills Regional High School, 108 Stirling Rd, Warren, NJ 07059. You should enter through the main entrance at the front of the school.</p>
       <br/>
     </Container>
   </>
