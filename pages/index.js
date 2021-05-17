d4a43af10a2a6b3d9799dbbc80f849f1ec18b5dc
googol88 <ronaldfleung@gmail.com>
2021-05-17 12:32:08 -0400

    💬 change content

--- pages/index.js
+++ pages/index.js
@@ -71,7 +71,7 @@ const Index = (props) => (
     <Container as="section" py={4} variant="copy" {...props}>
       <Heading variant="headline">This is not your typical hackathon.</Heading>
       <Text as="p" sx={{fontSize: 2}} mb={3}><b>hillsHacks was born with a mission:</b> to show that computer science is something anyone can learn. It's more than memorizing technical jargon and stuffing code into machines. CS is for innovators, creators, problem solvers, artists, and <i>you</i>.</Text>
-      <Text as="p" sx={{fontSize: 2}} mb={3}>Rather than leave you alone expecting a full-fledged product launch at the end of the day, we'll guide you through the learning process with workshops from Watchung Hills alumni. Plus, we'll provide you one-on-one mentorship and an opportunity to listen in and ask questions about tech during our Panel Q&A session. Deviating from the traditional hackathon, you'll implement your ideas in our design challenge without needing to code, so when you leave you'll be ready to put your new-found skills into action.</Text>
+      <Text as="p" sx={{fontSize: 2}} mb={3}>Rather than leave you alone expecting a full-fledged product launch at the end of the day, we'll guide you through the learning process with workshops from Watchung Hills alumni. Plus, we'll provide you one-on-one mentorship and an opportunity to listen in and ask questions about tech during our Panel Q&A session. Deviating from the traditional hackathon, there will be two tracks: code and no-code, with a greater emphasis on ideas and how technology can be used for good.</Text>
       <Text variant="lead"><b>This is the fourth iteration of hillsHacks, Watchung Hills' premier hackathon open to grades 6 through 12.</b></Text>
       <br/>
     </Container>
@@ -85,11 +85,11 @@ const Index = (props) => (
         <Box>
           <h2>Where and when will the event take place?</h2>
           <p>hillsHacks will run as a hybrid event. Watchung Hills students may attend in-person at the high school or virtually from home, but all other attendees must attend virtually via Zoom. Even though things will be different this year, we'll strive for an authentic experience that retains the spirit of hacking!</p>
-          <p>The event will take place on <b>Saturday, June 5, from 10:00 AM to 4:30 PM.</b></p>
+          <p>The event will take place on <b>Saturday, June 5, from 1:00 PM to 5:00 PM.</b></p>
         </Box>
         <Box>
           <h2>What if I don’t know anything about coding or computer science?</h2>
-          <p>No problem! We highly encourage beginners to come, and our workshops will give you opportunities to learn about fundamental CS concepts. After creating an idea for the design challenge, you'll be ready to implement it on your own!</p>
+          <p>No problem! We highly encourage beginners to come, and our workshops will give you opportunities to learn about fundamental CS concepts. After creating an idea as part of the no-code hackathon track, you'll be ready to implement it on your own!</p>
         </Box>
         <Box>
           <h2>I’m an expert at coding. What do I get out of this event?</h2>
